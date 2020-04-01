c3badc0898eb0e32a0ef9e09ad69b7da0f3f3fc5
Josh Yap <joshua.c.yap@gmail.com>
2020-03-31 23:21:18 -0500

    add in sleep timers to get around popups by pressing escape key

--- automate.py
+++ automate.py
@@ -1,22 +1,40 @@
 from selenium import webdriver
+from pynput.keyboard import Key, Controller
+import time
 import config
 # fetch facebook password from separate file in the same directory
 pw = config.secret['pw']
-
+keyboard = Controller()
 
 driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-notifications')
+
+# navigate to facebook url
 driver.get('https://facebook.com')
 # driver.get('https://youtube.com')
 
+# input email address
 email_input = driver.find_element_by_xpath('//*[@id="email"]')
 email_input.send_keys('joshua.c.yap@gmail.com')
-
+# input pw
 password_input = driver.find_element_by_xpath('//*[@id="pass"]')
 password_input.send_keys(pw)
-
+# click login button element
 login_button = driver.find_element_by_xpath('//*[@id="u_0_b"]')
 login_button.click()
 
 
-# driver.until()
-# driver.close()
+home_button = driver.find_element_by_xpath('//*[@id="u_0_c"]/a')
+# wait 8 seconds for browser popup before pressing esc to get out
+time.sleep(8)
+keyboard.press(Key.esc)
+
+
+# actual actions go here
+
+
+# close browser window after actions are complete
+time.sleep(5)
+driver.close()
